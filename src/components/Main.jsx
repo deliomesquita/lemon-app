7b05f841f32fc5afc125b3eb381bfddfce69b16c
Delio Mesquita <armajjet@gmail.com>
2024-07-31 12:50:29 +0200

    Created and added Specials Component and to Main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,43 +1,36 @@
 import "../styles/Main.css";
-import Cards from "./Cards";
+// import Cards from "./Cards";
+import Specials from "./Specials";
 import Testimonials from "./Testimonials";
 import About from "./About";
 import Footer from "./Footer";
 
 function Main() {
   return (
-    <> 
+    <>
       <main className="main__container__description">
         <div className="main">
-          <section className="main-intro-section">  
-              <div className="main-intro-text-block">
-                <h1 className="main-intro-text-title">Little Lemon</h1>
-                <h3>Chicago</h3>
-                <p>
+          <section className="main-intro-section">
+            <div className="main-intro-text-block">
+              <h1 className="main-intro-text-title">Little Lemon</h1>
+              <h3>Chicago</h3>
+              <p>
                 We are a family-owned restaurant that serves delicious food and
                 drinks. We are located in Lisbon, Portugal.
-                </p>
-                <button className="main-intro-button">Reserve a Table</button>
-              </div>
-              <div className="main-intro-image-block">
-                <img className="main-intro-image" src="\images\restauranfood.jpg" alt="Little Lemon Food" />
-              </div>
+              </p>
+              <button className="main-intro-button">Reserve a Table</button>
+            </div>
+            <div className="main-intro-image-block">
+              <img
+                className="main-intro-image"
+                src="\images\restauranfood.jpg"
+                alt="Little Lemon Food"
+              />
+            </div>
           </section>
         </div>
       </main>
-      <main className="main__container__specials">
-        <div className="main__specials">
-          <section className="main-specials-section">
-          <div className="specials-container">
-            <div className="specials-text-button">
-              <h2 className="main-specials-text-title">Specials</h2>
-              <button className="main-specials-button">Online Menu</button>
-            </div>
-            <Cards />
-          </div>
-        </section>
-        </div>
-      </main>
+      <Specials />
       <main className="main__container__testimonials">
         <Testimonials />
       </main>
---
+++ src/components/Specials.jsx
@@ -0,0 +1,20 @@
+import "../styles/Specials.css";
+import Cards from "./Cards";
+
+function Specials() {
+  return (
+    <section className="section__container__specials">
+      <article className="article-specials-section">
+        <div className="specials-container">
+          <div className="specials-text-button">
+            <h2 className="specials-text-title">Specials</h2>
+            <button className="specials-button">Online Menu</button>
+          </div>
+          <Cards />
+        </div>
+      </article>
+    </section>
+  );
+}
+
+export default Specials;
