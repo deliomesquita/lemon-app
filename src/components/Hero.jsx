65daaf3accc4daccbc25ff2f1e5c96fd2f2f2b39
Delio Mesquita <armajjet@gmail.com>
2024-08-01 10:52:46 +0200

    Hero component created

---
+++ src/components/Hero.jsx
@@ -0,0 +1,28 @@
+import "../styles/Hero.css";
+
+function Hero() {
+  return (
+    <section className="hero__container__description">
+      <article className="hero-intro-section">
+        <div className="hero-intro-text-block">
+          <h1 className="hero-intro-text-title">Little Lemon</h1>
+          <h3>Chicago</h3>
+          <p>
+            We are a family-owned restaurant that serves delicious food and
+            drinks. We are located in Lisbon, Portugal.
+          </p>
+          <button className="hero-intro-button">Reserve a Table</button>
+        </div>
+        <div className="hero-intro-image-block">
+          <img
+            className="hero-intro-image"
+            src="\images\restauranfood.jpg"
+            alt="Little Lemon Food"
+          />
+        </div>
+      </article>
+    </section>
+  );
+}
+
+export default Hero;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,5 @@
 import "../styles/Main.css";
-// import Cards from "./Cards";
+import Hero from "./Hero";
 import Specials from "./Specials";
 import Testimonials from "./Testimonials";
 import About from "./About";
@@ -8,28 +8,7 @@ import Footer from "./Footer";
 function Main() {
   return (
     <>
-      <main className="main__container__description">
-        <div className="main">
-          <section className="main-intro-section">
-            <div className="main-intro-text-block">
-              <h1 className="main-intro-text-title">Little Lemon</h1>
-              <h3>Chicago</h3>
-              <p>
-                We are a family-owned restaurant that serves delicious food and
-                drinks. We are located in Lisbon, Portugal.
-              </p>
-              <button className="main-intro-button">Reserve a Table</button>
-            </div>
-            <div className="main-intro-image-block">
-              <img
-                className="main-intro-image"
-                src="\images\restauranfood.jpg"
-                alt="Little Lemon Food"
-              />
-            </div>
-          </section>
-        </div>
-      </main>
+      <Hero />
       <Specials />
       <main className="main__container__testimonials">
         <Testimonials />
