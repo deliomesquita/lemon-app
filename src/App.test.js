6f8d11d3b3caeaa58f2636daa32aa921ff7797ed
Delio Mesquita <armajjet@gmail.com>
2024-11-11 16:26:47 +0100

    Added commented tests to test file

--- src/App.test.js
+++ src/App.test.js
@@ -7,69 +7,69 @@ import { cleanup } from "@testing-library/react";
 
 afterEach(cleanup);
 
-// // Mock the fetchAPI function to return a predefined array of times
-// jest.mock("./api", () => ({
-//   fetchAPI: jest.fn(),
-// }));
+// Mock the fetchAPI function to return a predefined array of times
+jest.mock("./api", () => ({
+  fetchAPI: jest.fn(),
+}));
 
-// // Reset mocks before each test
-// beforeEach(() => {
-//   jest.clearAllMocks();
-// });
+// Reset mocks before each test
+beforeEach(() => {
+  jest.clearAllMocks();
+});
 
-// // 1st test
+// 1st test
 
-// test("Validate the initial state of the reducer", () => {
-//   // Mock fetchAPI to return the expected times
-//   fetchAPI.mockReturnValueOnce([
-//     "17:00",
-//     "18:00",
-//     "19:00",
-//     "20:00",
-//     "21:00",
-//     "22:00",
-//   ]);
+test("Validate the initial state of the reducer", () => {
+  // Mock fetchAPI to return the expected times
+  fetchAPI.mockReturnValueOnce([
+    "17:00",
+    "18:00",
+    "19:00",
+    "20:00",
+    "21:00",
+    "22:00",
+  ]);
 
-//   const initialState = initializeTimes();
+  const initialState = initializeTimes();
 
-//   // Assert that the initial state matches the mock fetchAPI output
-//   expect(initialState).toEqual([
-//     "17:00",
-//     "18:00",
-//     "19:00",
-//     "20:00",
-//     "21:00",
-//     "22:00",
-//   ]);
-// });
+  // Assert that the initial state matches the mock fetchAPI output
+  expect(initialState).toEqual([
+    "17:00",
+    "18:00",
+    "19:00",
+    "20:00",
+    "21:00",
+    "22:00",
+  ]);
+});
 
-// // 2nd test
+// 2nd test
 
-// test("Validate the reducer updates the state", () => {
-//   // Create an initial state from initializeTimes
-//   fetchAPI.mockReturnValueOnce([
-//     "17:00",
-//     "18:00",
-//     "19:00",
-//     "20:00",
-//     "21:00",
-//     "22:00",
-//   ]);
+test("Validate the reducer updates the state", () => {
+  // Create an initial state from initializeTimes
+  fetchAPI.mockReturnValueOnce([
+    "17:00",
+    "18:00",
+    "19:00",
+    "20:00",
+    "21:00",
+    "22:00",
+  ]);
 
-//   const initialState = initializeTimes();
+  const initialState = initializeTimes();
 
-//   // Action that updates the state with new availableTimes
-//   const action = {
-//     type: "update_times",
-//     availableTimes: ["18:00", "19:00", "20:00"],
-//   };
+  // Action that updates the state with new availableTimes
+  const action = {
+    type: "update_times",
+    availableTimes: ["18:00", "19:00", "20:00"],
+  };
 
-//   // Call updateTimes with the initial state and the action
-//   const updateState = updateTimes(initialState, action);
+  // Call updateTimes with the initial state and the action
+  const updateState = updateTimes(initialState, action);
 
-//   // Assert that the updated state matches the action's availableTimes
-//   expect(updateState).toEqual(["18:00", "19:00", "20:00"]);
-// });
+  // Assert that the updated state matches the action's availableTimes
+  expect(updateState).toEqual(["18:00", "19:00", "20:00"]);
+});
 
 // Validation tests for the Booking form input values
 
